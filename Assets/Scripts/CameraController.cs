9c13ce4f12dc74684255551f3041a5e7d57d32ea
Marcus Mourão <Marcus Mouro>
2017-02-12 12:27:30 -0200

    Add script to rotate camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,81 +3,37 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-
 	public GameObject player;
-    float speed = 2f; //10f;
+	float speed = 10f;
 	private Vector3 offset;
 
 	void Start()
 	{
+		transform.rotation = Quaternion.Euler(0, 0, 0);
+		player.transform.rotation = Quaternion.Euler(0, (180), 0);
+
 		offset = transform.position - player.transform.position;
-    }
+	}
 
-    //private float speed = 2.0f;
-    /*    void Update()
-        {
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.position += Vector3.right * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.position += Vector3.left * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.position += Vector3.forward * speed * Time.deltaTime;
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.position += Vector3.back * speed * Time.deltaTime;
-            }
-        }*/
-
-    public float minX = -360.0f;
-    public float maxX = 360.0f;
-
-    public float minY = -45.0f;
-    public float maxY = 45.0f;
-
-    public float sensX = 500.0f;
-    public float sensY = 500.0f;
-
-    float rotationY = 0.0f;
-    float rotationX = 0.0f;
-
-    void LateUpdate()
+	void LateUpdate()
 	{
-        transform.position = player.transform.position;
 
-        /*
-        // Andar com a camera (independente do objeto) 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += Vector3.back * speed * Time.deltaTime;
-        }
+		transform.position = player.transform.position;
+		if (Input.GetKey(KeyCode.Q))
+		{
+			float my_y = transform.rotation.eulerAngles.y;
+			transform.rotation = Quaternion.Euler(0, (my_y - 5), 0);
+			player.transform.rotation = Quaternion.Euler(0, (my_y - 5), 0);
+		}
 
-        // Girar a camera
-        if (Input.GetMouseButton(0))
-        {
-            rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
-            rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
-            rotationY = Mathf.Clamp(rotationY, minY, maxY);
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }*/
-    }
+		if (Input.GetKey(KeyCode.E))
+		{
+			float my_y = transform.rotation.eulerAngles.y;
+			transform.rotation = Quaternion.Euler(0, (my_y + 5), 0);
+			player.transform.rotation = Quaternion.Euler(0, (my_y - 5), 0);
+
+		}
+
+	}
 }
 
