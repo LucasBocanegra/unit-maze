16f60b4a6a6959ea4849c65b852489075563e176
Yago Arroyo <yagoag@gmail.com>
2017-02-12 01:03:44 -0200

    Varios pickups, vence o jogo ao encontra 5 (e nao consegue mais se mover)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,19 +5,79 @@ public class CameraController : MonoBehaviour
 {
 
 	public GameObject player;
-	float speed = 10f;
+    float speed = 2f; //10f;
 	private Vector3 offset;
 
 	void Start()
 	{
 		offset = transform.position - player.transform.position;
-	}
+    }
 
-	void LateUpdate()
+    //private float speed = 2.0f;
+    /*    void Update()
+        {
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += Vector3.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += Vector3.back * speed * Time.deltaTime;
+            }
+        }*/
+
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 500.0f;
+    public float sensY = 500.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    void LateUpdate()
 	{
+        transform.position = player.transform.position;
 
-		transform.position = player.transform.position;
-		
-	}
+        /*
+        // Andar com a camera (independente do objeto) 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        // Girar a camera
+        if (Input.GetMouseButton(0))
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }*/
+    }
 }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,74 @@ public class PlayerController : MonoBehaviour
 	public float speed;
 	public Text countText;
 	public Text winText;
+    public GameObject pickup0, pickup1, pickup2, pickup3, pickup4, pickup5, pickup6;
 
 	private Rigidbody rb;
 	private int count;
+    private int pickupAtivo;
+    private ArrayList pickedup;
+    private bool ganhou;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 		count = 0;
+        ganhou = false;
         winText.enabled = false;
-	}
+
+        pickup0.SetActive(false);
+        pickup1.SetActive(false);
+        pickup2.SetActive(false);
+        pickup3.SetActive(false);
+        pickup4.SetActive(false);
+        pickup5.SetActive(false);
+        pickup6.SetActive(false);
+        pickedup = new ArrayList();
+
+        AtivarPickup();
+    }
+
+    void AtivarPickup()
+    {
+        int n;
+
+        do
+        {
+            n = Random.Range(0, 7);
+        } while (pickedup.Contains(n) && n == 7);
+
+        switch (n)
+        {
+            case 0:
+                pickup0.SetActive(true);
+                pickupAtivo = 0;
+                break;
+            case 1:
+                pickup1.SetActive(true);
+                pickupAtivo = 1;
+                break;
+            case 2:
+                pickup2.SetActive(true);
+                pickupAtivo = 2;
+                break;
+            case 3:
+                pickup3.SetActive(true);
+                pickupAtivo = 3;
+                break;
+            case 4:
+                pickup4.SetActive(true);
+                pickupAtivo = 4;
+                break;
+            case 5:
+                pickup5.SetActive(true);
+                pickupAtivo = 5;
+                break;
+            case 6:
+                pickup6.SetActive(true);
+                pickupAtivo = 6;
+                break;
+        }
+    }
 
 	void FixedUpdate()
 	{
@@ -26,7 +84,8 @@ public class PlayerController : MonoBehaviour
 
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce(movement * speed);
+        if (!ganhou)
+		    rb.AddForce(movement * speed);
 	}
 
     void OnTriggerEnter(Collider other)
@@ -36,10 +95,16 @@ public class PlayerController : MonoBehaviour
 			other.gameObject.SetActive(false);
 			count = count + 1;
             countText.text = "Itens encontrados: "+count.ToString();
+            pickedup.Add(pickupAtivo);
 
-            if (count > 0) // Mudar para o valor de objetos que deve encontrar!
+            if (count >= 5) // Mudar para o valor de objetos que deve encontrar!
             {
                 winText.enabled = true;
+                ganhou = true;
+            }
+            else
+            {
+                AtivarPickup();
             }
 		}
 	}
